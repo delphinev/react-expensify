89ca0269b84e3469a02aae193bcca86b2b21989e
Delphine Vilaysack <delphine.vilaysack@onepark.fr>
2018-02-18 16:11:38 +0100

    Comment dummy expenses data

--- src/app.js
+++ src/app.js
@@ -12,9 +12,9 @@ import { setTimeout } from 'core-js/library/web/timers';
 import 'react-dates/lib/css/_datepicker.css'
 
 const store = configureStore();
-store.dispatch(addExpense({description: 'Water bill', amount: 400}));
-store.dispatch(addExpense({description: 'Gas bill', amount: 150, createdAt: 1000}));
-store.dispatch(addExpense({description: 'Rent', amount: 1590, createdAt: 500}));
+// store.dispatch(addExpense({description: 'Water bill', amount: 400}));
+// store.dispatch(addExpense({description: 'Gas bill', amount: 150, createdAt: 1000}));
+// store.dispatch(addExpense({description: 'Rent', amount: 1590, createdAt: 500}));
 // store.dispatch(setTextFilter('water'));
 
 // setTimeout(() => {
@@ -25,9 +25,9 @@ store.dispatch(addExpense({description: 'Rent', amount: 1590, createdAt: 500}));
 //   store.dispatch(setTextFilter('bill'))
 // }, 3000)
 
-const state = store.getState();
-const visibleExpenses = getVisibleExpenses(state.expenses, state.filters);
-console.log(visibleExpenses);
+// const state = store.getState();
+// const visibleExpenses = getVisibleExpenses(state.expenses, state.filters);
+// console.log(visibleExpenses);
 
 const jsx = (
   <Provider store={store}>
